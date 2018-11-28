875551eddb371cee60d064e67fa735851d3b9f51
Dinar <dinar88raimov@gmail.com>
2018-11-28 13:59:24 +0300

    env var

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,18 +2,18 @@
 require 'rest-client'
 require 'json'
 class QuotesController < ApplicationController
-	api_url = ENV['STORAGE_URL']
+	@@api_url = ENV['STORAGE_URL']
 	def index
 		# =begin=begin=beginresponse = RestClient.get('localhost:3000/rest/quotes')
 	 # 	@quotes = JSON.parse(response)=end=end=end
 	end
 	def json
-		if api_url!=null
-			response = RestClient.get(api_url+'/json/quotes')
+		if @@api_url!=null
+			response = RestClient.get(@@api_url+'/json/quotes')
 		end
 		else
 			response = RestClient.get('localhost:3000/json/quotes')
-			
+
 	 	@quotes = JSON.parse(response)
 	 	render "json_quotes"
 	end
