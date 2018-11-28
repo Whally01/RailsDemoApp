71ac095f846c4da64480a212c2a519e1a3848d70
Dinar <dinar88raimov@gmail.com>
2018-11-28 13:55:17 +0300

    env var

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,23 +2,39 @@
 require 'rest-client'
 require 'json'
 class QuotesController < ApplicationController
+	api_url = ENV['STORAGE_URL']
 	def index
 		# =begin=begin=beginresponse = RestClient.get('localhost:3000/rest/quotes')
 	 # 	@quotes = JSON.parse(response)=end=end=end
 	end
 	def json
-		response = RestClient.get('localhost:3000/json/quotes')
+		if api_url!=null
+			response = RestClient.get(api_url+'/json/quotes')
+		end
+		else
+			response = RestClient.get('localhost:3000/json/quotes')
+			
 	 	@quotes = JSON.parse(response)
 	 	render "json_quotes"
 	end
 	def rest
-		response = RestClient.get('localhost:3000/rest/quotes')
+		if api_url!=null
+			response = RestClient.get(api_url+'/json/quotes')
+		end
+		else
+			response = RestClient.get('localhost:3000/json/quotes')
+			
 	 	@quotes = JSON.parse(response)
-	 	render "rest_quotes"
+	 	render "json_quotes"
 	end
 	def graphql
-		response = RestClient.get('localhost:3000/graphql/quotes')
+		if api_url!=null
+			response = RestClient.get(api_url+'/json/quotes')
+		end
+		else
+			response = RestClient.get('localhost:3000/json/quotes')
+			
 	 	@quotes = JSON.parse(response)
-	 	render "graphql_quotes"
+	 	render "json_quotes"
 	end
 end
