1a508fc542cc9dc20b43e36780a1d386ce7ec5a5
Dinar <dinar88raimov@gmail.com>
2018-11-28 14:12:35 +0300

    f

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,37 +3,38 @@ require 'rest-client'
 require 'json'
 class QuotesController < ApplicationController
 	@@api_url = ENV['STORAGE_URL']
+	puts @@api_url
 	def index
 		# =begin=begin=beginresponse = RestClient.get('localhost:3000/rest/quotes')
 	 # 	@quotes = JSON.parse(response)=end=end=end
 	end
 	def json
-		if @@api_url!=null
-			response = RestClient.get(@@api_url+'/json/quotes')
-		end
+		if @@api_url && @@api_url.empty?
+			url = @@api_url.concat("/json/quotes")
+			response = RestClient.get(url)
 		else
 			response = RestClient.get('localhost:3000/json/quotes')
-
+		end
 	 	@quotes = JSON.parse(response)
 	 	render "json_quotes"
 	end
 	def rest
-		if api_url!=null
-			response = RestClient.get(api_url+'/json/quotes')
-		end
+		if @@api_url && @@api_url.empty?
+			url = @@api_url.concat("/rest/quotes")
+			response = RestClient.get(url)
 		else
 			response = RestClient.get('localhost:3000/json/quotes')
-			
+		end
 	 	@quotes = JSON.parse(response)
 	 	render "json_quotes"
 	end
 	def graphql
-		if api_url!=null
-			response = RestClient.get(api_url+'/json/quotes')
-		end
+		if @@api_url && @@api_url.empty?
+			url = @@api_url.concat("/graphql/quotes")
+			response = RestClient.get(url)
 		else
 			response = RestClient.get('localhost:3000/json/quotes')
-			
+		end
 	 	@quotes = JSON.parse(response)
 	 	render "json_quotes"
 	end
